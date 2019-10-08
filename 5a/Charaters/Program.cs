bf8bbf01d3c1e5d562cf6b97b114e0d880bc750c
JohnnyVu2209 <johnnyv2209@gmail.com>
2019-10-08 15:25:07 +0700

    decorate

--- 5a/Charaters/Program.cs
+++ 5a/Charaters/Program.cs
@@ -10,22 +10,68 @@ namespace Charaters
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please input character you want");
-            Char c = Char.Parse(Console.ReadLine());
-            //PrintAtoZ();
-            //PrintZtoA();
-            //UpCaseAtoZ();
+            //Console.Write('A');
+            //char c = 'A';
+            //Console.WriteLine(c);
+            //short s = 0x41;
+            //Console.WriteLine((char)s);
+            //c = (char)s;
+            //Console.WriteLine(c);
+            //c++;
+            //Console.WriteLine(c);
+            Console.WriteLine("What you wanna do now??");
+            Console.WriteLine("We have 5 options for you");
+            Console.WriteLine("a.Print all English Alphabet letters from a to z");
+            Console.WriteLine("b.Print all English Alphabet letters from z to a");
+            Console.WriteLine("c.Print all English Alphabet letters from A to Z (UPPER CASE)");
+            Console.WriteLine("d.Output the ASCII code of an inputted character");
+            Console.WriteLine("e.Check if an inputted character is upper or lower case");
+            Console.WriteLine("f.Change an inputted character to upper and lower case");
 
-            Ascii(c);
-            IsUpperOrLowerCase(c);
-            ChangeToUpper(c);
-            ChangeToLower(c);
+            Console.WriteLine("You choose: ");
+            char choose = Convert.ToChar(Console.ReadLine());
+            Char c = ' ';
+            switch(choose){
+                case 'a':
+                    PrintAtoZ();
+                    break;
+                case 'b':
+                    PrintZtoA();
+                    break;
+                case 'c':
+                    UpCaseAtoZ();
+                    break;
+                case 'd':
+                     Console.WriteLine("Please input character you want");
+                     c = Char.Parse(Console.ReadLine());
+                     Ascii(c);
+                    break;
+                case 'e':
+                    Console.WriteLine("Please input character you want");
+                    c = Char.Parse(Console.ReadLine());
+                    IsUpperOrLowerCase(c);
+                    break;
+                case 'f':
+                    Console.WriteLine("Please input character you want");
+                    c = Char.Parse(Console.ReadLine());
+                    ChangeToUpperAndLower(c);
+                    break;
+                default:
+                    Console.WriteLine("dont have in options we give you");
+                    break;
+            }
+       
         }
         static void PrintAtoZ()
         {
+            
+            //for (short i = 0; i <256 ; i++)
+            //{
+            //    Console.Write((char)i + " ");
+            //}
             for (char c = 'a'; c <= 'z'; c++)
             {
-                Console.Write(c);
+                Console.Write(c + " ");
             }
             Console.WriteLine();
         }
@@ -33,7 +79,7 @@ namespace Charaters
         {
             for (char c = 'z'; c >= 'a'; c--)
             {
-                Console.Write(c);
+                Console.Write(c+ " ");
             }
             Console.WriteLine();
         }
@@ -41,7 +87,7 @@ namespace Charaters
         {
             for (char c = 'A'; c <= 'Z'; c++)
             {
-                Console.Write(c);
+                Console.Write(c + " ");
             }
             Console.WriteLine();
         }
@@ -54,13 +100,7 @@ namespace Charaters
         {
             return Convert.ToInt32(c);
         }
-        //static void CheckCharater(char c)
-        //{
-        //    if (IsUpperOrLowerCase(c) == true)
-        //        Console.WriteLine("the character you give is Upper");
-        //    else
-        //        Console.WriteLine("the character you give is Lower");
-        //}
+       
         static void IsUpperOrLowerCase(char c)
         {
             if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
@@ -78,18 +118,28 @@ namespace Charaters
 
 
         }
+        static void ChangeToUpperAndLower(char c)
+        {
+            if (c >= 'a' && c <= 'z')
+                ChangeToUpper(c);
+            if (c >= 'A' && c <= 'Z')
+                ChangeToLower(c);
+            else
+                Console.WriteLine("cant do it");
+        }
         static void ChangeToUpper(char c)
         {
 
             if ((c >= 'a' && c <= 'z'))
             {
                 c = (char)(c - 'a' + 'A');
+                Console.WriteLine("Here is UPPER charater you want: {0} ", c);
             }
             else if ((c >= 'A' && c <= 'Z'))
-                c = c;
+                Console.WriteLine("Here is UPPER charater you want: {0} ", c);
             else
                 Console.WriteLine("the character you give is Not in letter characters ");
-            Console.WriteLine("Here is UPPER charater you want: {0} ", c);
+            
         }
         static void ChangeToLower(char c)
         {
@@ -97,12 +147,13 @@ namespace Charaters
             if ((c >= 'A' && c <= 'Z'))
             {
                 c = (char)(c - 'A' + 'a');
+                Console.WriteLine("Here is Lower charater you want: {0} ", c);
             }
             else if ((c >= 'a' && c <= 'z'))
-                c= c;
+                Console.WriteLine("Here is Lower charater you want: {0} ", c);
             else
                 Console.WriteLine("the character you give is Not in letter characters ");
-            Console.WriteLine("Here is Lower charater you want: {0} ", c);
+            
         }
     }
 }
