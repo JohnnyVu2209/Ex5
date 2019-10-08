025f664683bc40a54e5a2ffa6934ab55ea6f8fd7
JohnnyVu2209 <johnnyv2209@gmail.com>
2019-10-08 12:24:24 +0700

    decorate

--- 5b/Program.cs
+++ 5b/Program.cs
@@ -10,17 +10,42 @@ namespace _5b
     {
         static void Main(string[] args)
         {
-            //String input = Convert.ToString(Console.ReadLine());
-            //IssueB1(input);
-            //Console.Write("please enter value of x: ");
-            //String[] x = Console.ReadLine().Split(' ');
-            //int n = x.Length;
-            //int[] array = new int[n];
-            //InputInt(x , array);
-            //FindMaxMin(array);
-            //String Nhap = Convert.ToString(Console.ReadLine());
-            //Console.WriteLine("this is the result when processed: {0}", ChangeToUpperOrLowerCase(Nhap)); 
-            Draw(20);
+            Console.WriteLine("What you wanna do now??");
+            Console.WriteLine("We have 4 optionS for you");
+            Console.WriteLine("1.check if an inputted string is palindrome or not.");
+            Console.WriteLine("2.input n integers on the same line (separated by space), then output the maximum and minimum numbers.");
+            Console.WriteLine("3.change a line of string (inputted by user) to uppercase or lowercase");
+            Console.WriteLine("4.display the following figure of height h.");
+            String Ask = Convert.ToString(Console.ReadLine());   
+            if(Ask == "1")
+            {
+                Console.WriteLine("Please input a word, phrase, number, or other sequence of characters ");
+                String input = Convert.ToString(Console.ReadLine());
+                IssueB1(input);
+            }
+            else if(Ask == "2")
+            {
+                Console.Write("please enter value of x: ");
+                String[] x = Console.ReadLine().Split(' ');
+                int n = x.Length;
+                int[] array = new int[n];
+                InputInt(x, array);
+                FindMaxMin(array);
+            }
+            else if(Ask == "3")
+            {
+                String Nhap = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("this is the result when processed: {0}", ChangeToUpperOrLowerCase(Nhap));
+            }
+            else if(Ask == "4")
+            {
+                Console.WriteLine("Please insert the height of Drawing you want");
+                int h = Convert.ToInt16(Console.ReadLine());
+                Draw(h);
+            }
+            else
+                Console.WriteLine("OUT OF OPTIONS!!!!");
+            
          
         }
         static bool Palindrome(String input)
@@ -117,7 +142,7 @@ namespace _5b
             {
                 for (int j = 0; j < h; j++)
                 {
-                    Console.Write(cArr[i,j]);
+                    Console.Write(cArr[i,j] + "");
                 }
                 Console.WriteLine();
             }
