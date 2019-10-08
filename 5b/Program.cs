b2662cb8583835bf8fd2b722e6208f1262e75ad9
JohnnyVu2209 <johnnyv2209@gmail.com>
2019-10-08 10:40:18 +0700

    Input done , FindMaxMin done.

--- 5b/Program.cs
+++ 5b/Program.cs
@@ -10,8 +10,15 @@ namespace _5b
     {
         static void Main(string[] args)
         {
-            String input = Convert.ToString(Console.ReadLine());
-            IssueB1(input);
+            //String input = Convert.ToString(Console.ReadLine());
+            //IssueB1(input);
+            Console.Write("please enter value of x: ");
+            String[] x = Console.ReadLine().Split(' ');
+            int n = x.Length;
+            int[] array = new int[n];
+            InputInt(x , array);
+            FindMaxMin(array);
+         
         }
         static bool Palindrome(String input)
         {
@@ -33,6 +40,42 @@ namespace _5b
             else
                 Console.WriteLine("An inputted String is NOT Palindrome ");
         }
+        static void InputInt(String[] x , int[] array)  
+        {
+           
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(x[i] );
+            }
+            Console.WriteLine("Input successful");
+            
+        }
+        static int Max(int[] a)
+        {
+            int Max = a[0];
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (Max <= a[i])
+                    Max = a[i];
+            }
+            return Max;
+        }
+        static int Min(int[] a)
+        {
+            int Min = a[0];
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (Min >= a[i])
+                    Min = a[i];
+            }
+            return Min;
+        }
+        static void FindMaxMin(int[] a)
+        {
+            Console.WriteLine("the Max number is {0}" ,Max(a));
+            Console.WriteLine("the Min number is {0}", Min(a));
+        }
+
      
     }
 }
