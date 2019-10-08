291ed89053db7ec07f5b7392ee67b320ae233ec1
JohnnyVu2209 <johnnyv2209@gmail.com>
2019-10-08 09:42:30 +0700

    Palindrome

---
+++ 5b/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String input = Convert.ToString(Console.ReadLine());
+            IssueB1(input);
+        }
+        static bool Palindrome(String input)
+        {
+            String palindrome = "";
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                palindrome += input[i];
+            }
+            if(palindrome == input)
+            {
+                return true;
+            }
+            return false;
+        }
+        static void IssueB1(String input)
+        {
+            if (Palindrome(input) == true)
+                Console.WriteLine("An inputted String is Palindrome ");
+            else
+                Console.WriteLine("An inputted String is NOT Palindrome ");
+        }
+     
+    }
+}
