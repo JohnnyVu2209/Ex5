14f0395ffcc5c3e81a8a354ca32ac80708164c2b
JohnnyVu2209 <johnnyv2209@gmail.com>
2019-10-10 15:28:32 +0700

    game

---
+++ 5c/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _5c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String f = @"C:\187pm14039\Ex5\Text.txt";
+
+            string testData = File.ReadAllText(f);
+           
+                Console.WriteLine(testData);
+            
+        }
+    }
+}
---
+++ PickBallGame/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PickBallGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static void PrintGame()
+        {
+        }
+        static void Get1Group(int[] game , out int g)
+        {
+            g = 0;
+            if (game[1] > 0 && game[2] == 0 && game[3] == 0)
+                g = 1;
+            if (game[1] == 0 && game[2] > 0 && game[3] == 0)
+                g = 2;
+            if (game[1] == 0 && game[2] == 0 && game[3] > 0)
+                g = 3;
+        }
+        static bool Has3Group(int[] game )
+        {
+            return 
+        }
+
+    }
+}
